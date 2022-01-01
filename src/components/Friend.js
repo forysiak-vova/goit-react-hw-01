6cc82910995a9f1b285cf6168efc4560ac72ab84
forysiak-vova <volodja.forus@gmail.com>
2022-01-01 11:08:33 +0100

    e

--- src/components/Friend.js
+++ src/components/Friend.js
@@ -1,20 +1,18 @@
 import PropTypes from 'prop-types';
 
-
-function Friend({name, status, avatar }) {
-   return (
-
-<div className="item">
-         <span className="status">{status ? 'Онлайн' : 'Офлайн'}</span>
-         <img className="avatar" src={avatar} alt={name} width="48" />
-         <span className="name">{name}</span>
-</div>
-   )
+function Friend({ name, status, avatar }) {
+  return (
+    <div className="item">
+      <span className="status">{status ? 'Онлайн' : 'Офлайн'}</span>
+      <img className="avatar" src={avatar} alt={name} width="48" />
+      <p className="name">{name}</p>
+    </div>
+  );
 }
- 
+
 Friend.propTypes = {
-   name: PropTypes.string,
-   avatar: PropTypes.string,
-   status: PropTypes.bool,
-}
+  name: PropTypes.string,
+  avatar: PropTypes.string,
+  status: PropTypes.bool,
+};
 export default Friend;
